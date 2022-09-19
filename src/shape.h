4320853380c6c07fe14597066a913f8c25f4735f
Enor <suyn1314@>
2022-09-20 05:46:03 +0800

    Refactor code.

--- src/shape.h
+++ src/shape.h
@@ -3,9 +3,8 @@
 class Shape
 {
 public:
-    //Area With Polymorphism Should Be Correct
+    //Polymorphism
     virtual double area() const = 0;
-    //Perimeter With Polymorphism Should Be Correct
     virtual double perimeter() const = 0;
     virtual std::string info() const = 0;
     virtual ~Shape() {}
